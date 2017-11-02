15146a9a4a2bde034c83bac2f69704aa9704a015
lijunxia <364893724@qq.com>
2017-11-02 10:24:59 +0800

    修改了key为字符串是加密

--- AES128/AES128/LJXAES128.h
+++ AES128/AES128/LJXAES128.h
@@ -34,6 +34,23 @@
  @param data 需要加密的string
  @return 加密后的字符串
  */
++ (NSString *)AES128EncryptkeyString:(NSData *)data;
+
+
+/**
+ 解密
+ 
+ @param string 加密的字符串
+ @return 解密后的内容
+ */
++ (NSString *)AES128DecryptkeyString:(NSString *)string;
+
+/**
+ 加密
+ 
+ @param data 需要加密的string
+ @return 加密后的字符串
+ */
 //+ (NSData *)AES128EncryptData:(NSData *)data;
 //
 //
